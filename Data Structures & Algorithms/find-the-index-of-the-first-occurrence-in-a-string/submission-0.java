e6fd084f01861644d29e303a7b6bf2c91ae96a95
Ishant <ishant.7123217@gmail.com>
2026-04-06 04:12:17 +0530

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-0

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack == null || needle == null || haystack.length() == 0
+        || needle.length() == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < (haystack.length()-needle.length()+1); i++) {
+            int j = 0;
+            while (j < needle.length() && needle.charAt(j) == haystack.charAt(i+j)) {
+                j++;
+            }
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
