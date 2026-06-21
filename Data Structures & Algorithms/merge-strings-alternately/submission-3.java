6f10330553f83d872bbce25ce559d3887e63fa7a
Ishant <ishant.7123217@gmail.com>
2026-06-21 10:24:43 +0530

    Add: merge-strings-alternately - submission-3

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0;
+        int len1 = word1.length();
+        int len2 = word2.length();
+        StringBuilder sb  = new StringBuilder();
+        while ( i < len1 || i < len2) {
+            if (i < len1) {
+                sb.append(word1.charAt(i));
+            }
+            if (i < len2) {
+                sb.append(word2.charAt(i));
+            }
+            i++;
+        }
+        return sb.toString();
+    }
+}
