ef80d38f4a7977a174d277c3479548d628e6990d
Ishant <ishant.7123217@gmail.com>
2026-07-09 03:20:10 +0530

    Add: sort-colors - submission-5

---
+++ Data Structures & Algorithms/sort-colors/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int s = 0;
+        int e = nums.length-1;
+        int i = 0;
+        while (i<=e && s < e) {
+            if (nums[i] == 0) {
+                nums[i] = nums[s];
+                nums[s] = 0;
+                i++;
+                s++;
+            } else if (nums[i] == 2) {
+                nums[i] = nums[e];
+                nums[e] = 2;
+                e--;
+            } else {
+                i++;
+            }
+        }
+    }
+}
